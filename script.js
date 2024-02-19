c68813f87956c9411c4f9772ffb5887effe61e56
JB957 <101916980+JB957@users.noreply.github.com>
2024-02-19 00:10:25 -0500

    Added a hover effect

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container")
 makeGrid()
+
 function makeGrid(){
     for(let r = 0; r < 16; r ++){
         for(let c = 0; c < 16; c ++){
@@ -8,6 +9,10 @@ function makeGrid(){
             content.style.border = 'black 1px solid';
             content.style.height = '100px'
             content.style.width = '100px'
+            content.addEventListener('mouseenter', () => 
+            {
+                content.style.backgroundColor = 'black';
+            })
             container.appendChild(content);
         }
     }
