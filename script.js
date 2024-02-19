aeb0d43477e060ff2faf518f3ce18ab1315840e2
JB957 <101916980+JB957@users.noreply.github.com>
2024-02-19 13:26:27 -0500

    Event listen so when button is click prompts user to enter number of sides

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const container = document.querySelector("#container")
-makeGrid()
+const body = document.querySelector("body")
+const height = screen.height
+const btn = document.querySelector('#btn')
+createGrid(16)
 
-function makeGrid(){
-    for(let r = 0; r < 16; r ++){
-        for(let c = 0; c < 16; c ++){
-            const content = document.createElement('div')
-            content.classList.add('content')
-            content.style.border = 'black 1px solid';
-            content.style.height = '100px'
-            content.style.width = '100px'
-            content.addEventListener('mouseenter', () => 
-            {
-                content.style.backgroundColor = 'black';
+btn.addEventListener('click', () => {
+    let numSquare = Number(prompt("Enter a Number of Squares per side"));
+    if(numSquare <= 100){
+        container.replaceChildren();
+        createGrid(numSquare)
+    }  
+})
+
+function createGrid(numSquare){
+    for (let i = 0; i < numSquare; i++) {
+        let row = document.createElement('div');
+        row.style.height = `${height/numSquare}px`;
+        row.style.width = `${height}px`;
+        row.style.display = 'flex';
+        container.appendChild(row);
+
+        for (let j = 0; j < numSquare; j++) {
+            let col = document.createElement('div');
+            col.classList.add("box")
+            col.style.height = `${height/numSquare}px`;
+            col.style.width = `${height/numSquare}px`;
+            col.style.border = "black 1px solid"
+            col.addEventListener("mouseenter" , () => {
+                col.style.backgroundColor = "black";
             })
-            container.appendChild(content);
+            row.appendChild(col);
         }
     }
 }
