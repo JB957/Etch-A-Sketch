12eb165ccb4bc418426b35e7d640b8007337264a
JB957 <101916980+JB957@users.noreply.github.com>
2024-02-18 23:56:20 -0500

    make grid function boiler plate code

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container")
+makeGrid()
+function makeGrid(){
+    for(let r = 0; r < 16; r ++){
+        for(let c = 0; c < 16; c ++){
+            const content = document.createElement('div')
+            content.classList.add('content')
+            content.style.border = 'black 1px solid';
+            content.style.height = '100px'
+            content.style.width = '100px'
+            container.appendChild(content);
+        }
+    }
+}
